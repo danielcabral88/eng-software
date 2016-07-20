2a27371ec48a7292f84a451cc25d81259c1fe8dd
danielcabral88 <dc.pessoal@gmail.com>
2016-07-20 20:16:05 -0300

    Teste de integação final

---
+++ br.com.engsoft/br.com.engsoft.teste/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("br.com.engsoft.teste")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("810daef8-5bd3-4579-8773-4603626505df")]
---
+++ br.com.engsoft/br.com.engsoft.teste/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using br.com.engsoft;
+
+namespace br.com.engsoft.teste
+{
+    public class Tests
+    {
+        //private readonly Program _brcomengsoft; 
+            
+    }
+}
